a53fed954f72ea997cd5201b44b328a37b241ed6
ners1us <66124861+ners1us@users.noreply.github.com>
2025-04-13 14:49:14 +0300

    feat: add port for metrics server

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,18 +3,20 @@ package config
 import "os"
 
 type Config struct {
-	DbUrl     string
-	JWTSecret string
-	RestPort  string
-	GrpcPort  string
+	DbUrl          string
+	JWTSecret      string
+	RestPort       string
+	GrpcPort       string
+	PrometheusPort string
 }
 
 func NewConfig() *Config {
 	return &Config{
-		DbUrl:     getEnv("DB_URL"),
-		JWTSecret: getEnv("JWT_SECRET"),
-		RestPort:  getEnv("REST_PORT"),
-		GrpcPort:  getEnv("GRPC_PORT"),
+		DbUrl:          getEnv("DB_URL"),
+		JWTSecret:      getEnv("JWT_SECRET"),
+		RestPort:       getEnv("REST_PORT"),
+		GrpcPort:       getEnv("GRPC_PORT"),
+		PrometheusPort: getEnv("PROMETHEUS_PORT"),
 	}
 }
 
